e5f3e28fd50b32f9f7725b1dc08f5ca701f2014f
ayo <fenyodi.andras@gmail.com>
2025-12-08 12:42:47 +0100

    -merged with main -subject_events moved to timersutils and imported in for more spacious code -for naming convention with indexes I dropped the 4th subject col from the dataframe and now with that the iterations of subject data should align with the naming indexes: 0-9 idx -> 0-9 subject names -

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import matplotlib.pyplot as plt
 
 # EMG imports:
 from backbone import Subject
-from timesrsutils import TimeSrsTools
+from timesrsutils import TimeSrsTools, SUBJECT_EVENTS
 
 # PPG imports:
 from pyPPG import Fiducials
@@ -18,7 +18,7 @@ from PPG_section.PPG_handle import PPGProcTools
 RECORDER_PPG_FS = int(28057 / 140)
 PPG_TIME_ARR = np.arange(0, 28057) / RECORDER_PPG_FS
 DO_PPG_PLOT = False  # disable live plotting for batch PPG processing
-PPG_SEGMENT = (0, 140)  # segment to visualize
+PPG_SEGMENT = (0, 140)  # segment in seconds to visualize
 
 # Output directory for PPG
 PPG_BASE_DIR = os.path.join("temp_dir", "PPG_AllSubjects")
@@ -80,6 +80,8 @@ def process_ppg_subject(index, signal):
     save_data(s=s, fp=fp_new, bm=bm, savingformat='csv', savingfolder=subject_dir)
 
     print(f"Finished PPG Subject {index}")
+    # return values usefull for further processing
+    return s, fp, fid #, bm_defs, bm_vals, bm_stats, bm, fp_new
 
 if __name__ == '__main__':
 
@@ -87,85 +89,14 @@ if __name__ == '__main__':
     print("\n=== STARTING EMG PROCESSING ===")
 
     ids = ['P2', 'P3', 'P4', 'P5', 'P6', 'P7', 'P8', 'P9', 'P10']
-    subject_events = {
-        "P2": [
-            {"phase": "rest", "start_s": 0.0, "end_s": 4.2640},
-            {"phase": "lift", "start_s": 4.2640, "end_s": 17.9740},
-            {"phase": "rest", "start_s": 17.9740, "end_s": 28.3400},
-            {"phase": "lift", "start_s": 28.3400, "end_s": 42.1960},
-            {"phase": "rest", "start_s": 42.1960, "end_s": 54.3080},
-            {"phase": "lift", "start_s": 54.3080, "end_s": 68.5500},
-        ],
-        "P3": [
-            {"phase": "rest", "start_s": 0.0, "end_s": 4.3520},
-            {"phase": "lift", "start_s": 4.3520, "end_s": 17.9660},
-            {"phase": "rest", "start_s": 17.9660, "end_s": 26.3900},
-            {"phase": "lift", "start_s": 26.3900, "end_s": 40.6820},
-            {"phase": "rest", "start_s": 40.6820, "end_s": 48.1880},
-            {"phase": "lift", "start_s": 48.1880, "end_s": 62.6000},
-        ],
-        "P4": [
-            {"phase": "rest", "start_s": 0.0, "end_s": 5.0020},
-            {"phase": "lift", "start_s": 5.0020, "end_s": 20.8540},
-            {"phase": "rest", "start_s": 20.8540, "end_s": 26.4480},
-            {"phase": "lift", "start_s": 26.4480, "end_s": 43.6560},
-            {"phase": "rest", "start_s": 43.6560, "end_s": 50.6500},
-            {"phase": "lift", "start_s": 50.6500, "end_s": 66.7580},
-        ],
-        "P5": [
-            {"phase": "rest", "start_s": 0.0, "end_s": 4.5480},
-            {"phase": "lift", "start_s": 4.5480, "end_s": 20.0400},
-            {"phase": "rest", "start_s": 20.0400, "end_s": 28.4940},
-            {"phase": "lift", "start_s": 28.4940, "end_s": 45.4440},
-            {"phase": "rest", "start_s": 45.4440, "end_s": 52.5660},
-            {"phase": "lift", "start_s": 52.5660, "end_s": 69.3460},
-        ],
-        "P6": [
-            {"phase": "rest", "start_s": 0.0, "end_s": 4.6600},
-            {"phase": "lift", "start_s": 4.6600, "end_s": 18.6860},
-            {"phase": "rest", "start_s": 18.6860, "end_s": 27.7940},
-            {"phase": "lift", "start_s": 27.7940, "end_s": 42.8460},
-            {"phase": "rest", "start_s": 42.8460, "end_s": 51.2260},
-            {"phase": "lift", "start_s": 51.2260, "end_s": 67.7320},
-        ],
-        "P7": [
-            {"phase": "rest", "start_s": 0.0, "end_s": 2.7880},
-            {"phase": "lift", "start_s": 2.7880, "end_s": 20.0240},
-            {"phase": "rest", "start_s": 20.0240, "end_s": 28.9380},
-            {"phase": "lift", "start_s": 28.9380, "end_s": 45.5820},
-            {"phase": "rest", "start_s": 45.5820, "end_s": 53.2720},
-            {"phase": "lift", "start_s": 53.2720, "end_s": 67.0000},
-        ],
-        "P8": [
-            {"phase": "rest", "start_s": 0.0, "end_s": 12.7400},
-            {"phase": "lift", "start_s": 12.7400, "end_s": 28.1120},
-            {"phase": "rest", "start_s": 28.1120, "end_s": 37.1860},
-            {"phase": "lift", "start_s": 37.1860, "end_s": 51.2420},
-            {"phase": "rest", "start_s": 51.2420, "end_s": 58.5760},
-            {"phase": "lift", "start_s": 58.5760, "end_s": 73.1480},
-        ],
-        "P9": [
-            {"phase": "rest", "start_s": 0.0, "end_s": 4.5920},
-            {"phase": "lift", "start_s": 4.5920, "end_s": 18.8180},
-            {"phase": "rest", "start_s": 18.8180, "end_s": 26.7700},
-            {"phase": "lift", "start_s": 26.7700, "end_s": 40.2180},
-            {"phase": "rest", "start_s": 40.2180, "end_s": 48.6620},
-            {"phase": "lift", "start_s": 48.6620, "end_s": 63.0940},
-        ],
-        "P10": [
-            {"phase": "rest", "start_s": 0.0, "end_s": 4.9460},
-            {"phase": "lift", "start_s": 4.9460, "end_s": 18.6860},
-            {"phase": "rest", "start_s": 18.6860, "end_s": 27.9280},
-            {"phase": "lift", "start_s": 27.9280, "end_s": 43.1300},
-            {"phase": "rest", "start_s": 43.1300, "end_s": 51.8840},
-            {"phase": "lift", "start_s": 51.8840, "end_s": 65.1800},
-        ]
-    }
+    # moved subject events to timersutils as a global constant it seemed fitting. More spacious code.
+    subject_events = SUBJECT_EVENTS
+
     # PART 1b: lift to rest segmentation
     subjects_emg: dict[str, Subject] = {}
     for subject_id in ids:
         # Note: data folder should exist
-        path = os.path.join("local/data", f"BSL-EMG-Book{subject_id}.csv")
+        path = os.path.join("local", "data", f"BSL-EMG-Book{subject_id}.csv")
         subject_event = subject_events[subject_id]
         # where padding
         subj = Subject.from_csv(name=subject_id, path=path, fs=FS_EMG, target_len=EMG_LEN)
@@ -187,11 +118,12 @@ if __name__ == '__main__':
     plt.tight_layout()
     plt.show()
 
-    # call emg_preprocess for all
+    # TODO call emg_preprocess for all
 
 
     # PART 2: PPG Processing:
     print("\n=== STARTING PPG PROCESSING ===")
+
     # Load full dataset
     # Note: change this to ourData.csv location
     ppg_data_path = "./local/data/ourData.csv"
@@ -200,24 +132,16 @@ if __name__ == '__main__':
 
     # Load full dataset
     df = pd.read_csv(ppg_data_path)
-    df = df.drop(columns=["tstamp"], errors="ignore")
+    df = df.drop(columns=["tstamp", "padArr_4"], errors="ignore")  # drop sub 4 right away -> indexing will be better
 
-    # Build list of subject signals
-    signals = []
-
-    patient_arrays = [arr for idx, arr in enumerate(df.to_numpy()) if idx != 4]
-    # align patient naming convention
-    for idx, (key, subject) in enumerate(subjects_emg.items()):
-        events = subject_events[key]
-        arr = patient_arrays[idx]
-
-        subject = subject.resample_data(200)
-        sig = PPGProcTools.create_dotmaps_for_pyPPG(arr, str(idx + 2), RECORDER_PPG_FS)
-        signals.append(sig)
-
+    # Process all subjects
+    for idx, sig in enumerate(df.to_numpy()):
         try:
-            process_ppg_subject(idx + 2, sig)
+            sig = PPGProcTools.create_dotmaps_for_pyPPG(sig, ids[idx], RECORDER_PPG_FS)
 
+            process_ppg_subject(ids[idx], sig)
+            # process_ppg_subject(idx + 2, sig)
+            # TODO process_ppg_subject should return signal as is
             signal = PPGProcTools.preprocess_signal(sig)
 
             # Filter out first 5 seconds
@@ -240,8 +164,12 @@ if __name__ == '__main__':
             passive_windows_rms = [np.sqrt(np.mean(window["Integrated EMG"] ** 2)) for window in subject.passive_windows]
             passive_windows_mav = [np.mean(np.abs(window["Integrated EMG"])) for window in subject.passive_windows]
             passive_windows_ppg_amplitude = [np.max(window["ppg"]) for window in subject.passive_windows]
+            sig = PPGProcTools.create_dotmaps_for_pyPPG(sig, ids[idx], RECORDER_PPG_FS)
+            process_ppg_subject(ids[idx], sig)
         except Exception as e:
+            # TODO: choose correct warning
             print(f" Error processing PPG subject {idx+2}: {e}")
+            print(f" Error processing PPG subject {ids[idx]}: {e}")
             continue
 
     print("\nAll PPG subjects processed.")
